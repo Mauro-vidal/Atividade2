31e056540e52b03f41ff7860d0d36ebdbd4cbc1a
Mauro-vidal <mauroviidal@gmail.com>
2024-03-19 17:04:02 -0300

    Tarefa 1 alteração.

--- src/Album.java
+++ src/Album.java
@@ -28,12 +28,30 @@ public class Album {
       return musicas;
   }
 
+    //    @Override
+    //    public String toString() {
+    //      return "Album{" +
+    //                "nome='" + nome + '\'' +
+    //                ", anoLancamento=" + anoLancamento +
+    //                ", musicas=" + musicas +
+    //                '}';
+    //    }
+
     @Override
     public String toString() {
-        return "Album{" +
-                "nome='" + nome + '\'' +
-                ", anoLancamento=" + anoLancamento +
-                ", musicas=" + musicas +
-                '}';
+        StringBuilder sb = new StringBuilder();
+        sb.append("Album{");
+        sb.append("nome='").append(nome).append("', ");
+        sb.append("anoLancamento=").append(anoLancamento).append(", ");
+        sb.append("musicas=[");
+        for (Musica musica : musicas) {
+            sb.append(musica.getNome()).append(", ");
+        }
+        if (!musicas.isEmpty()) {
+            sb.setLength(sb.length() - 2); // Remove a última vírgula e espaço
+        }
+        sb.append("]}");
+        return sb.toString();
     }
+
 }
