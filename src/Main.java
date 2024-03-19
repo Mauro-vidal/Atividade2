17094be7bfc304bd9b711b10762d339bfc257838
Mauro-vidal <mauroviidal@gmail.com>
2024-03-19 17:11:34 -0300

    Tarefa 3 implementada.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrayList<Musica> musicas = new ArrayList<>();
+
+        musicas.add(new Musica("Good Vibe"));
+        musicas.add(new Musica("Ilusão"));
+        musicas.add(new Musica("O preço"));
+        musicas.add(new Musica("Iluminado"));
+        musicas.add(new Musica("life"));
+
+
+        Date anoLancamento = new Date(2024, 6, 9);
+
+        Album album = new Album("Caminhos", anoLancamento, musicas);
+        System.out.println(album);
+
     }
 }
